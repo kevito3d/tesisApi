f8ecf2810bca2109bba8e777b71f188ffd18f707
kevito3d <kcovena5034@utm.edu.ec>
2021-11-21 12:46:05 -0500

    add middleware cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express  from "express";
 import morgan from 'morgan';
+import cors from 'cors'
 
 //import rooutes
 import plant from './routes/plant'
@@ -9,7 +10,18 @@ const app= express();
 
 //middlewares
 app.use(morgan('dev'))
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+  });
+
 app.use(express.json());
+app.use(cors());
 
 //routes
 app.use('/api/plant', plant);
