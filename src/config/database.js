cf8a36ac1c2df9eb90802bad557346d30817cd61
kevito3d <kevincove98@gmail.com>
2022-10-27 20:13:52 -0500

    change directory

---
+++ src/config/database.js
@@ -0,0 +1,23 @@
+import Sequelize from "sequelize";
+import 'dotenv/config'
+export const sequelize = new Sequelize(
+    process.env.db_name,
+    process.env.db_user,
+    process.env.db_password,
+    {
+        host: process.env.db_host,
+        dialect: process.env.db,
+
+        /* dialectOptions: {
+            client_encoding: 'UTF8'
+            
+        }, */
+        pool: {
+            max: 5,
+            min: 0,
+            require: 30000,
+            idle: 10000,
+        },
+        logging: false
+    }
+)
